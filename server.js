327b7dc9b68e3967d98fe4ceeafe3f97d8fb5be9
ringol5046 <ringol5046@gmail.com>
2017-08-29 23:20:02 -0500

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.log('Unable to append to server.log.')
+		}
+	});
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>hello express</h1>');
-    res.send({
-        name: 'ringo',
-        likes: [
-            'movie', 
-            'game',
-            'football'
-        ]
-    });
+	res.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome'
+	});
 });
 
 app.get('/about', (req, res) => {
-    res.send('about page');
+	res.render('about.hbs', {
+		pageTitle: 'About page'
+	});
 });
 
 app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: "unable to handle request"
-    })
-})
+	res.send({
+		errorMessage: "unable to handle request"
+	});
+});
 
-app.listen(3000, () => {
-    console.log('server is up on port number 3000')
+app.listen(port, () => {
+	console.log(`server is up on port number ${port}`)
 });
